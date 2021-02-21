9468ad1c2eb13d50888dfea12cd53eb6e791d297
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2021-02-21 21:32:19 +0600

    almost done

--- app.js
+++ app.js
@@ -2,19 +2,28 @@ const apiKey = 'b6cc4392568a3586e950307c86a22bbd';
 const apiBase = 'https://api.openweathermap.org/data/2.5/weather';
 
 const getWeatherData = city => {
-    const url = `${apiBase}?q=${city}&units=metric&appid=${apiKey}`
+    const url = `${apiBase}?q=${city}&units=metric&appid=${apiKey}`;
     fetch(url)
     .then (response => response.json())
     .then (data => updateWeather(data))
+    console.log(url);
 }
 
 const searchBtn = document.getElementById('search_button');
 searchBtn.addEventListener('click', () => {
     const inputCity = document.getElementById('input-city').value;
-    getInputData(inputCity);
+    getWeatherData(inputCity);
 })
 
 
-updateWeather = data => {
-    document.getElementById('show-city').innerText = data
+const updateWeather = data => {
+    document.getElementById('show-city').innerText = data.name || "Unknown Location!";
+    document.getElementById('show-temperature').innerText = data.main.temp;
+    console.log(data.main);
+    console.log(data.weather[0].icon);
+
+    document.getElementById('icon').setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+    document.getElementById('input-city').value = "";
 }
+
+getWeatherData('Dhaka');
